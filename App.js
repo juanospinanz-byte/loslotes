88ad4c7be0740388aac17ee2b463a37697b7ce5a
Juanes <juan.ospinanz@amigo.edu.co>
2026-02-22 13:42:49 -0500

    se agrega formulario y actualizo app.js

--- App.js
+++ App.js
@@ -1,10 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import Formulario from './src/components/Formulario';
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>aplicacion</Text>
+      <Formulario/>
       <StatusBar style="auto" />
     </View>
   );

8ce12251ff191746a0803ccb82aa011beef39044
Juanes <juan.ospinanz@amigo.edu.co>
2026-02-28 08:30:40 -0500

    se agrega el colors y se agraga la paleta de colores

---
+++ src/constants/colors.js
@@ -0,0 +1,9 @@
+export default{
+    //colores principales y variantes
+    principal : '#003B5F',
+    variante1: '#1998CC',
+    variante2: '#146A96',
+
+    //colores para el texto y las sombras
+    default: '#08854D'
+}

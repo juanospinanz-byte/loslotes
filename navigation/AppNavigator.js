bf85ef6a3358a12c78be067302dcb8e89f4628e4
Juanes <juan.ospinanz@amigo.edu.co>
2026-03-07 09:42:31 -0500

    se pone extensiones y se va configurano

---
+++ navigation/AppNavigator.js
@@ -0,0 +1,75 @@
+import React, { useEffect, useState } from 'react';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { createStackNavigator } from '@react-navigation/stack';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import SplashScreen from '../src/screens/SplashScreen';
+import RegisterScreen from '../src/screens/auth/RegisterScreen';
+import LoginScreen from '../src/screens/auth/LoginScreen';
+
+const Tab = createBottomTabNavigator();
+const Stack = createStackNavigator();
+const ProfileStack = createNativeStackNavigator();
+
+const TabNavigator = () =>{
+    const {user} = useAuth();
+    return(
+        <Tab.Navigator initialRouteName='Home'
+        screenOptions ={(({route}))}=>({
+            tabBarIcon: ({color, size, focused}) =>{
+                let IconName;
+                if (route.name === "Home"){
+                    iconName = focused ? 'home' : 'home-outline';
+                }else if(route.name === "settings"){
+                    IconName = focused ? 'settings' : 'settings-outline';
+                }else if(route.name === "User"){
+                    IconName = focused ? 'person' : 'person-outline';
+                }
+        })>
+        </Tab.Navigator>
+    )
+}
+
+const AuthNavigator = () =>{
+    reutrn (
+        <Stack.Navigator screenOptions={{headerShown: false}}>
+            <Stack.Screen name='Splash' component={SplashScreen} />
+            <Stack.Screen name='Register' component={RegisterScreen} />
+            <Stack.Screen name='Login' component={LoginScreen} />
+        </Stack.Navigator>
+    )
+}
+
+const AppNavigator = () => {
+    const [user, setUser] = useState(null);
+    const [isLoading, setIsLoading] = useState(true);
+
+    useEffect(() => {
+        const unsuscribe = onAuthStateChanged(auth, (user) =>{
+            setUser(user);
+            setIsLoading(False);
+        });
+        return () => unsuscribe();
+    }, []);
+
+    const authContextValue ={
+        user,
+        setUser,
+        isLoading,
+        setIsLoading,
+    };
+
+    if(isLoading){
+        return <SplashScreen/>; //el null se reemplazara por el splash screen
+    }
+
+    return(
+        <authContextValue.Provider value={authContetValue}>
+            <Stack.Navigator initialRouteName='Splash'>
+                <Stack.Screen name='Splash' component = {SplashScreen} />
+                <Stack.Screen name='Register' component = {RegisterScreen} />
+                <Stack.Screen name='Login' component = {LoginScreen} />
+
+            </Stack.Navigator>
+        </authContextValue.Provider>
+    );
+}

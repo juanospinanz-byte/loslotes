620032cf414e8a98733e50fbd530f756a8870f07
Juanes <juan.ospinanz@amigo.edu.co>
2026-03-14 09:38:33 -0500

    se actualiza

--- App.js
+++ App.js
@@ -13,12 +13,3 @@ export default function App() {
     </AppProvider>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
